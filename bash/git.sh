4c0afd4a17f65ac575cbaa7c08297bdfca50885c
Alexis Sukrieh <sukria@gmail.com>
2023-11-03 12:17:04 +0100

    add gfs and gff alias for git flow feature management

--- bash/git.sh
+++ bash/git.sh
@@ -25,3 +25,28 @@ function gitexp ()
     echo $tar
 }
 
+function git_flow_feature_start() {
+    curbranch=$(git symbolic-ref --short HEAD)
+    if [ "$curbranch" == "devel" ]; then
+        if [ -z "$1" ]; then
+            echo "Please specify a feature name to start."
+        else
+            git flow feature start "$1"
+        fi
+    else
+        echo "You can only start a new feature from the 'devel' branch."
+    fi
+}
+
+function git_flow_feature_finish() {
+    curbranch=$(git symbolic-ref --short HEAD)
+    if [[ $curbranch == feature/* ]]; then
+        git flow feature finish "${curbranch#feature/}"
+    else
+        echo "You can only finish a feature if you are on a 'feature/*' branch."
+    fi
+}
+
+alias gfs="git_flow_feature_start"
+alias gff="git_flow_feature_finish"
+
